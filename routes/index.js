fb3f7113f2732e43ca865bd618284da155614c0d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-28 16:38:47 +0530

    Add middleware for post route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import UserRoutes from "./userRoutes.js";
+import PostRoutes from "./postRoutes.js";
 
 const router = Router();
 
 router.use("/api/user", UserRoutes);
+router.use("/api/posts", PostRoutes);
 
 export default router;
