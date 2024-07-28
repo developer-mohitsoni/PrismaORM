1986743d0d3f127c93546f67dad133165ddcba5a
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-28 12:42:25 +0530

    Add middleware for JSON and URL encoding
    This commit adds middleware for JSON and URL encoding to the server.js file. The middleware is configured using the `express.json()` and `express.urlencoded()` functions. This change ensures that the server can properly handle JSON and URL-encoded data in incoming requests.
    
    The purpose of this change is to enhance the server's functionality by enabling support for JSON and URL-encoded data.

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//* Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   return res.send("Hi, Everyone");
 });
