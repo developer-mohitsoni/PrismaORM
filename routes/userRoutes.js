15f81f895a6dc4c063904a153aa9716b8c0f7411
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-28 13:12:25 +0530

    Add showUser route from UserController

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,13 @@ import {
   createUser,
   updateUser,
   fetchUsers,
+  showUser,
 } from "../Controller/UserController.js";
 
 const router = Router();
 
-router.get("/", fetchUsers)
+router.get("/", fetchUsers);
+router.get("/", showUser);
 router.post("/", createUser);
 router.put("/:id", updateUser);
 
