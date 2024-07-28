efecd83a1059306ec4a1c6911ee9e022cb15b263
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-28 17:00:49 +0530

    Include Post data when fetching users
    This commit modifies the `fetchUsers` function in the `UserController.js` file to include the `Post` data along with the user data. This change allows for a more comprehensive view of the users and their associated posts and comment.

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,17 +1,27 @@
 import prisma from "../DB/db.config.js";
 
 //* To get all User data
+
+export const onlyFetchUser = async(req,res)=>{
+  const users = await prisma.user.findMany();
+
+  return res.status(200).json({
+    data: users,
+  });
+}
+
 export const fetchUsers = async (req, res) => {
   //* Now this below include shows userData along with Post
   const users = await prisma.user.findMany({
-    include: {
-      Post: {
+    //* To check the number of post,comment count by user
+    select:{
+      _count: {
         select: {
-          title: true,
-          comment_count: true,
-        },
-      },
-    },
+          Post: true,
+          Comment: true
+        }
+      }
+    }
   });
 
   return res.status(200).json({
