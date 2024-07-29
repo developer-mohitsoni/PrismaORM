ec7d880cdd89046062883cab96c232ab9fac1440
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-29 12:29:13 +0530

    code update

--- DB/db.config.js
+++ DB/db.config.js
@@ -2,13 +2,12 @@
 
 //* This class is the main entry point for interacting with your database using Prisma.
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 const prisma = new PrismaClient({
   //* It enables logging of SQL queries for debugging or performance monitoring.
 
   log: ["query"],
-}).$extends(withAccelerate());
+});
 
 //* It enables logging of SQL queries for debugging or performance monitoring.
 export default prisma;
